c71dc721770963125b3a1a6d5a5394b74c0ea674
Jeffrey Abraham <101375286+JeffreyTheVoyager@users.noreply.github.com>
2023-04-17 15:42:57 -0400

    login page

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
+const { config } = require('dotenv')
 const express = require('express')
+const sqlite3 = require('sqlite3').verbose()
 require('dotenv').config()
 
 const app = express()
+
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+
+/* Home */
+app.get('/', (req, res)=>{
+    res.send(`Hello World`)
+})
+
+
+/* Login */
+app.get('/login', (req, res)=>{
+    res.render('login.pug')
+})
+
+app.post('/login', (req, res)=>{
+    const {name} = req.body
+    insertStudent(name)
+    res.redirect('/')
+})
+
 
 const PORT = process.env.PORT || 5000;
 
